e23aefa8a0bf44a278f073c64eeb430d584dd0c8
Kunal Mehta <legoktm@member.fsf.org>
2018-01-20 20:26:51 -0800

    Round before checking if things have changed

--- src/Diff.php
+++ src/Diff.php
@@ -48,7 +48,9 @@ class Diff {
 		foreach ( $oldFiles as $path => $oldInfo ) {
 			if ( isset( $newFiles[$path] ) ) {
 				$newInfo = $newFiles[$path];
-				if ( $oldInfo !== $newInfo ) {
+				// Check equality after rounding, because we don't care
+				// about changes past 2 decimals, its meaningless
+				if ( round( $oldInfo, 2 ) !== round( $newInfo, 2 ) ) {
 					$changed[] = $path;
 				}
 			}
